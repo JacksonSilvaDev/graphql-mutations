d46c7687d728d9c83938da3865edc85baf6b554b
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-11-23 14:29:27 -0300

    Edit de usuario pela mutation

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -31,5 +31,19 @@ module.exports = {
         const excluidos = usuarios.splice(i, 1)
 
         return excluidos ? excluidos[0] : null
+    },
+    alterarUsuario(_, args) {
+        const i = usuarios.findIndex(u => u.id === args.id);
+
+        if (i < 0) throw new Error('Usuário não encontrado');
+
+        const usuario = {
+            ...usuarios[i],
+            ...args
+        }
+
+        usuarios.splice(i, 1, usuario)
+
+        return usuario;
     }
 }
